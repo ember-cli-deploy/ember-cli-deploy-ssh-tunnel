df3b392a0e12a1cf21944c2f68d3eaa10acc9f73
Mattia Gheda <ghedamat@gmail.com>
2015-08-29 15:50:11 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,78 @@
 /* jshint node: true */
 'use strict';
 
+var Promise    = require('ember-cli/lib/ext/promise');
+var fs         = require('fs');
+var tunnelSsh = require('tunnel-ssh');
+var untildify  = require('untildify');
+
+var DeployPluginBase = require('ember-cli-deploy-plugin');
+
+var MAX_PORT_NUMBER = 65535;
+var MIN_PORT_NUMBER = 49151;
+
 module.exports = {
-  name: 'ember-cli-deploy-ssh-tunnel'
+  name: 'ember-cli-deploy-ssh-tunnel',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = DeployPluginBase.extend({
+      name: options.name,
+      defaultConfig: {
+          dstPort: 6379,
+          srcPort: function() {
+            var range = MAX_PORT_NUMBER - MIN_PORT_NUMBER + 1;
+            return Math.floor(Math.random() * range) + MIN_PORT_NUMBER;
+          },
+          privateKeyPath: '~/.ssh/id_rsa',
+          tunnelClient: function(context) {
+            // if you want to provide your own ssh client to be used instead of one from this plugin
+            return context.tunnelClient || tunnelSsh;
+          }
+      },
+
+      requiredConfig: ['host', 'username'],
+
+      willDeploy: function(/* context */) {
+        var srcPort = this.readConfig('srcPort');
+
+        if (srcPort > MAX_PORT_NUMBER || srcPort < MIN_PORT_NUMBER) {
+          throw 'Port ' + srcPort + ' is not available to open a SSH connection on.\n' + 'Please choose a port between ' + MIN_PORT_NUMBER + ' and ' + MAX_PORT_NUMBER + '.';
+        }
+
+        var sshConfig = {
+          host: this.readConfig('host'),
+          dstPort: this.readConfig('dstPort'),
+          username: this.readConfig('username'),
+          localPort: srcPort,
+          privateKey: this.readConfig('privateKeyPath')
+        };
+
+        if (sshConfig.privateKey) {
+          sshConfig.privateKey = fs.readFileSync(untildify(sshConfig.privateKey));
+        }
+
+        var tunnel = this.readConfig('tunnelClient');
+
+        return new Promise(function (resolve, reject) {
+          var sshTunnel = tunnel(sshConfig, function (error /*, result */) {
+            if (error) {
+              reject(error);
+            } else {
+              resolve({
+                tunnel: {
+                  handler: sshTunnel,
+                  srcPort: srcPort
+                }
+              });
+            }
+          });
+        });
+      },
+
+      didDeploy: function(context) {
+        context.tunnel.handler.close();
+      }
+    });
+    return new DeployPlugin();
+  }
 };
---
+++ tests/unit/index-nodetest.js
@@ -0,0 +1,208 @@
+'use strict';
+
+var Promise = require('ember-cli/lib/ext/promise');
+var assert  = require('ember-cli/tests/helpers/assert');
+var fs  = require('fs');
+var path  = require('path');
+
+describe('ssh-tunnel plugin', function() {
+  var subject, mockUi;
+
+  beforeEach(function() {
+    subject = require('../../index');
+    mockUi = {
+      messages: [],
+      write: function() { },
+      writeLine: function(message) {
+        this.messages.push(message);
+      }
+    };
+  });
+
+  it('has a name', function() {
+    var result = subject.createDeployPlugin({
+      name: 'test-plugin'
+    });
+
+    assert.equal(result.name, 'test-plugin');
+  });
+
+  it('implements the correct hooks', function() {
+    var result = subject.createDeployPlugin({
+      name: 'test-plugin'
+    });
+
+    assert.equal(typeof result.defaultConfig, 'object');
+    assert.equal(typeof result.requiredConfig, 'object');
+    assert.equal(typeof result.willDeploy, 'function');
+    assert.equal(typeof result.didDeploy, 'function');
+  });
+
+  describe('configure hook', function() {
+
+    it('resolves if config is ok', function() {
+      var plugin = subject.createDeployPlugin({
+        name: 'ssh-tunnel'
+      });
+
+      var context = {
+        ui: mockUi,
+        config: {
+          'ssh-tunnel': {
+            host: 'example.com',
+            username: 'ghedamat'
+          }
+        }
+      };
+
+      plugin.beforeHook(context);
+      plugin.configure(context);
+      assert.ok(true); // it didn't throw
+    });
+
+    describe('without providing config', function () {
+      var plugin, context, config;
+
+      beforeEach(function() {
+        plugin = subject.createDeployPlugin({
+          name: 'ssh-tunnel'
+        });
+      });
+
+      it('raises about missing required config', function() {
+        config = { };
+        context = {
+          ui: mockUi,
+          config: config
+        };
+        plugin.beforeHook(context);
+        assert.throws(function(error){
+          plugin.configure(context);
+        });
+        var messages = mockUi.messages.reduce(function(previous, current) {
+          if (/- Missing required config:\s.*/.test(current)) {
+previous.push(current);
+          }
+
+          return previous;
+        }, []);
+        assert.equal(messages.length, 1);
+      });
+
+      it('warns about missing optional config', function() {
+        context = {
+          ui: mockUi,
+          config: {
+            'ssh-tunnel': {
+              host: 'example.com',
+              username: 'ghedamat'
+            }
+          }
+        };
+        plugin.beforeHook(context);
+        plugin.configure(context);
+        var messages = mockUi.messages.reduce(function(previous, current) {
+          if (/- Missing config:\s.*, using default:\s/.test(current)) {
+            previous.push(current);
+          }
+
+          return previous;
+        }, []);
+        assert.equal(messages.length, 4);
+      });
+
+      it('adds default config to the config object', function() {
+        context = {
+          ui: mockUi,
+          config: {
+            'ssh-tunnel': {
+              host: 'example.com',
+              username: 'ghedamat'
+            }
+          }
+        };
+        plugin.beforeHook(context);
+        plugin.configure(context);
+        assert.isDefined(config['ssh-tunnel'].dstPort);
+        assert.isDefined(config['ssh-tunnel'].srcPort);
+        assert.isDefined(config['ssh-tunnel'].tunnelClient);
+        assert.isDefined(config['ssh-tunnel'].privateKeyPath);
+      });
+    });
+  });
+
+  describe('willDeploy hook', function() {
+    var plugin;
+    var context;
+
+    beforeEach(function() {
+      plugin = subject.createDeployPlugin({
+        name: 'ssh-tunnel'
+      });
+
+      context = {
+        ui: mockUi,
+        config: {
+          'ssh-tunnel': {
+            host: 'example.com',
+            username: 'ghedamat',
+            srcPort: 50000,
+            dstPort: 6379,
+            tunnelClient: function() {
+              return function(config, cbk) {
+                process.nextTick(cbk, false, true);
+                return {
+                  close: function() { }
+                };
+              };
+            }
+          }
+        },
+      };
+      plugin.beforeHook(context);
+    });
+
+    it('returns the updated context if tunnel is successful', function(done) {
+      assert.isFulfilled(plugin.willDeploy(context)).then(function(result) {
+        assert.isDefined(result.tunnel.handler);
+        assert.isDefined(result.tunnel.srcPort);
+        done();
+      }).catch(function(reason) {
+        done(reason.actual.stack);
+      });
+    });
+  });
+
+  describe('didDeploy hook', function() {
+    var plugin;
+    var context;
+
+    beforeEach(function() {
+      plugin = subject.createDeployPlugin({
+        name: 'ssh-tunnel'
+      });
+
+      context = {
+        ui: mockUi,
+        config: {
+          'ssh-tunnel': {
+          }
+        }
+      };
+      plugin.beforeHook(context);
+    });
+
+    it('closes the tunnel', function(done) {
+      context.tunnel = {
+        handler: {
+          close: function() {
+            assert.ok(true);
+            done();
+          }
+        }
+      };
+
+      var result = plugin.didDeploy(context);
+    });
+  });
+});
