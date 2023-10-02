0bee90abac1ab3b1a2f8f5bc5a3c3346b3af0593
Luke Melia <luke@lukemelia.com>
2023-10-01 23:18:00 -0400

    Update CI, dependencies & release process

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,54 @@
 module.exports = {
   root: true,
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module'
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      configFile: false,
+      // your babel options
+      presets: ["@babel/preset-env"],
+    },
   },
-  extends: 'eslint:recommended',
+  plugins: [
+    'ember'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],
   env: {
-    node: true
+    browser: true
   },
   rules: {
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/**/*.js'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+        'ember/avoid-leaking-state-in-ember-objects': 'off'
+      })
+    }
+  ]
 };
